50281b5f3c26390fbd3cde635cc673965d8f48b0
Spatiume <78424296+Spatiume@users.noreply.github.com>
2021-09-26 19:48:11 +0300

    Add files via upload
    add slidermenu (boxlsider) , add order validate

--- test3.js
+++ test3.js
@@ -1,69 +1,127 @@
-const left = document.querySelector(".slidermenu__arrow-scroll--left");
-const right = document.querySelector(".slidermenu__arrow-scroll--right");
-const items = document.querySelector(".slidermenu__items");
+// // slidermenu my
 
-const step = 100;
-const maxRight = 700;
-const minRight = 0;
+// const left = document.querySelector(".slidermenu__arrow-scroll--left");
+// const right = document.querySelector(".slidermenu__arrow-scroll--right");
+// const items = document.querySelector(".slidermenu__items");
 
-let currentRight=0;
+// right.addEventListener("click", function (e) {
+//   loop("right", e);
+// });
 
+// left.addEventListener("click", function (e) {
+//   loop("left", e);
+// });
 
+// function loop(direction, e) {
+//   e.preventDefault();
 
-right.addEventListener("click", function (e) {
-  loop("right", e);
+//   if (direction === "right") {
+//     items.appendChild(items.firstElementChild);
+//   } else {
+//     items.insertBefore(items.lastElementChild,
+//       items.firstElementChild);
+//   }
+// };
+
+// box slider
+$(document).ready(function () {
+  $('.slidermenu__items').bxSlider();
 });
 
-left.addEventListener("click", function (e) {
-  loop("left", e);
-});
-
-function loop(direction, e) {
-  e.preventDefault();
-
-  if (direction === "right") {
-      items.appendChild(items.firstElementChild);
-           
-
-    } else {
-    items.insertBefore(items.lastElementChild,
-      items.firstElementChild);
-  }
-};
+// fullscreen menu
 
 const hamburgerMenu = document.querySelector(".hamburger-menu-link");
-const fullScreenMenu= document.querySelector(".fullscreen-menu");
-const fullScreenMenuClose= document.querySelector(".fullscreen-menu__close");
+const fullScreenMenu = document.querySelector(".fullscreen-menu");
+const fullScreenMenuClose = document.querySelector(".fullscreen-menu__close");
 
-hamburgerMenu.addEventListener("click", function(e){
+hamburgerMenu.addEventListener("click", function (e) {
   e.preventDefault();
   fullScreenMenu.style.display = "flex";
 });
 
-fullScreenMenuClose.addEventListener("click", function(e){
+fullScreenMenuClose.addEventListener("click", function (e) {
   e.preventDefault();
   fullScreenMenu.style.display = "none";
 });
 
-$(".menu__link").on('click', e =>{
-  $(fullScreenMenu).css({'display': 'none'});
+$(".menu__link").on('click', e => {
+  $(fullScreenMenu).css({ 'display': 'none' });
 })
 
-    let teamItems = $('.team__item');
-    let teamItemLink = $('.team__link');
 
-    $('.team__link').on('click', (e)=>{
-      e.preventDefault();
 
-      $(e.currentTarget).closest('.team__item').toggleClass('team__item--active');
-      $(e.currentTarget).closest('.team__item').siblings('.team__item').removeClass('team__item--active');      
-    })
+// team section
+let teamItems = $('.team__item');
+let teamItemLink = $('.team__link');
 
-    
+$('.team__link').on('click', (e) => {
+  e.preventDefault();
 
-    $('.burgermenu__trigger').on('click', e =>{
-      e.preventDefault();
+  $(e.currentTarget).closest('.team__item').toggleClass('team__item--active');
+  $(e.currentTarget).closest('.team__item').siblings('.team__item').removeClass('team__item--active');
+})
 
-      $(e.currentTarget).closest('.burgermenu__item').toggleClass('burgermenu__item--active');
-      $(e.currentTarget).closest('.burgermenu__item').siblings('.burgermenu__item').removeClass('burgermenu__item--active');
-    })
+//burger menu
+
+$('.burgermenu__trigger').on('click', e => {
+  e.preventDefault();
+
+  $(e.currentTarget).closest('.burgermenu__item').toggleClass('burgermenu__item--active');
+  $(e.currentTarget).closest('.burgermenu__item').siblings('.burgermenu__item').removeClass('burgermenu__item--active');
+})
+
+// order menu
+
+const orderForm = document.querySelector('.orderForm');
+const orderBtn = document.querySelector('#order__btn');
+
+orderBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  // console.log(orderForm.elements.name.value);
+  if (validateForm(orderForm)) {
+    // console.log('vsew ok');
+    const data = {
+      name: orderForm.elements.name.value,
+      phone: orderForm.elements.phone.value,
+      comment: orderForm.elements.comment.value,
+      to: 'some-mail@mail.com' // По заданию сервер принимает четыре параметра, включая email. 
+      // Но по макету в форме email он отсутствует.
+    }
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.send(JSON.stringify(data));
+    xhr.addEventListener('load', () => {
+      console.log(xhr.response.message);
+    });
+  }
+});
+
+function validateForm(form) {
+  let valid = true;
+
+  if (!validateField(form.elements.name)) {
+    valid = false;
+  }
+
+  if (!validateField(form.elements.phone)) {
+    valid = false;
+  }
+
+  if (!validateField(form.elements.comment)) {
+    valid = false;
+  }
+
+  return valid;
+}
+
+function validateField(field) {
+  // field.nextElementSibling.textContent = field.validationMessage;
+  if (field.checkValidity()) {
+    $(field).css('border-color', 'transparent');
+  } else {
+    $(field).css('border-color', 'red');
+  }
+  return field.checkValidity();
+}

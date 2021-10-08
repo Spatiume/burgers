0db446f7d98c6dd78d8c601c1b65dc9736fa5923
Spatiume <78424296+Spatiume@users.noreply.github.com>
2021-10-09 01:22:28 +0300

    onepagescroll, mobiledetect, touchswipe, resizemobile

--- test3.js
+++ test3.js
@@ -1,4 +1,6 @@
 // We listen to the resize event
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
